155df32ff28aba6ee3ff31c6235964d7dc103754
Eracler Dio <ilasaz1988@gmail.com>
2022-03-31 17:20:26 +0300

    fixed on bag 2 page adaptive

--- heros/hero.js
+++ heros/hero.js
@@ -1,88 +1,62 @@
-let cardPerson = document.querySelectorAll(".card__person")
-cardPerson.forEach((elem) => {
-    elem.addEventListener("click", showCard)
-})
 let cover_div = document.createElement("div") //Создаем div перекрывающий всю страницу кроме формы
+let close = document.createElement("div") //Создаем иконку крестика
+
+let cards__persones = document.querySelectorAll(".cards__persones-2") //Перебираем все разделы с карточками героев 
+let carouse_2 = document.querySelectorAll(".carouse_2") //Перебираем подразделы с карточками героев
+
+
+document.body.addEventListener("click", showCard)
 
 function showCard(e) {
 
+    if (!e.target.closest(".card__person")) return //если клик не по персонажу ничего не происходит
 
+    cards__persones.forEach((elem => elem.style.position = "static")) //обнуляем свойство position у раздела с карточками героев что бы модальное окно всплыло по центру экрана
+    carouse_2.forEach((elem => elem.style.transform = "none")) //обнуляем свойство transform у раздела с карточками героев что бы модальное окно всплыло по центру экрана
+
+    //фон по карточкой с информацией запрещающий кликать по элементам пока карточка открыта
     cover_div.classList.add("cover_div")
     document.body.append(cover_div)
 
-    let card = e.target.closest(".card__person")
-    infoPerson = card.lastElementChild
-    document.body.prepend(infoPerson)
+    //обращаемся к карточке с информацией
+    infoPerson = e.target.closest(".card__person").lastElementChild
     infoPerson.style.display = "flex"
 
     // Кнопка закрытия окна
-    close = document.createElement("div")
     close.classList.add("infoPerson_close")
     infoPerson.append(close)
-
+    close.hidden = false
     close.style.top = 0 + "px"
     close.style.left = (infoPerson.clientWidth) - (close.getBoundingClientRect().width) + "px"
-}
 
-//Запускаем функцию закрытия окна с информацией при клике на иконку
-document.body.addEventListener("click", closeShowCard)
-
-function closeShowCard(e) {
+    //События после клика на крестик
     if (e.target.closest(".infoPerson_close")) {
         cover_div.classList.remove("cover_div")
         infoPerson.style.display = "none"
-        close.style.display = "none"
+        close.hidden = true
         console.log(e.target)
+        cards__persones.forEach((elem => elem.style.position = "relative"))
+        carouse_2.forEach((elem => elem.style.transform = "translate(0rem, 0rem)"))
     }
 }
+
 
 //Прокрутка персонажей на маленьких экранах
 
 let arrow_1 = document.querySelectorAll(".arrow_1")
 let arrow_2 = document.querySelectorAll(".arrow_2")
-let carouse_2 = document.querySelectorAll(".carouse_2")
-let wrap_carouse_2 = document.querySelector("#wrap_carouse_2")
-
-// for (elem of carouse_2) {
-//     elem.setAttribute("value", 0)
-// }
 
 for (elemBtnLeft of arrow_1) {
     elemBtnLeft.addEventListener("click", leftCards)
-        // elemBtnLeft.setAttribute("value", 0)
 }
 for (elemBtnRight of arrow_2) {
     elemBtnRight.addEventListener("click", rightCards)
 }
-// arrow_1.addEventListener("click", leftCards)
-// arrow_2.addEventListener("click", rightCards)
-
-let num = 0
 
 function leftCards(e) {
-    // console.log(wrap_carouse_2.getBoundingClientRect().left)
-    // console.log(carouse_2.getBoundingClientRect().left)
-    // n = e.target.parentElement.firstElementChild.dataset.value
-    // num -= 5
-
     e.target.parentElement.firstElementChild.style.transform = `translate(${e.target.parentElement.firstElementChild.dataset.value -= 5}rem, 0rem)`
-
-    // if (num <= 0) {
-    //     num = 0
-    // }
-    // console.log(num)
 }
 
 function rightCards(e) {
-
-
-    // console.log(wrap_carouse_2.getBoundingClientRect().left + carouse_2.getBoundingClientRect().width)
-    // console.log(carouse_2.getBoundingClientRect().left + carouse_2.getBoundingClientRect().width)
-
     e.target.parentElement.firstElementChild.style.transform = `translate(${e.target.parentElement.firstElementChild.dataset.value = +e.target.parentElement.firstElementChild.dataset.value + 5}rem, 0rem)`
-    console.log(e.target.parentElement.firstElementChild)
-        // if (num >= 0) {
-        //     num = 0
-        // }
-
 }
