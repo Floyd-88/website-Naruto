958bfd502902c6bf747f8568952fb9c73ab4909c
Eracler Dio <ilasaz1988@gmail.com>
2022-04-01 15:07:03 +0300

    adaptive 3 page gallery, open big img

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -2,126 +2,153 @@ let section = document.querySelector(".section_gallery")
 let containerMain = document.querySelector(".wrapper_main_gallery")
 let container = document.querySelector(".wrapper__gallery")
 
+waveSrc = [] //Массив для картинок идущих снизу вверх
+waveSrcUp = [] //Массив для картинок идущих сверху вниз
+imgLoc = "img/w"
+j = 1
 
-waveSrc = [],
-    imgLoc = "img/w",
-    j = 1;
-
+//Получаем ширину и высоту экрана
 let screenWidth = window.screen.width;
 let screenHeight = window.screen.height;
 
+//Получаем случайное число
 function getRandomInRange(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+//Функция для картинок идущих вверх
 function createImg() {
     for (var i = 0; i < 50; i++) {
+        //Создаем картинки
         img = document.createElement("img")
         img.src = imgLoc + (i + 1) + ".jpg";
 
-        x = getRandomInRange(-1000, window.screen.width - 500);
-        y = getRandomInRange(-500, 1000);
+        //Присваем 3м переменным случайное значение в диапозоне
+        x = getRandomInRange(-800, window.screen.width - 700);
+        y = getRandomInRange(500, -1000);
         z = getRandomInRange(300, 2000);
 
-        img.style.transform = "translate3d(" + x + "px, " + 0 + "px, -" + z + "px)";
+        //Размещаем картинки
+        img.style.transform = "translate3d(" + x + "px, " + y + "px, -" + z + "px)";
         img.style.cursor = "pointer"
         container.append(img);
         waveSrc.push(img)
     }
-
 }
 
-document.body.addEventListener("click", bigSizeImg)
-document.body.addEventListener("click", smallSizeImg)
+//Функция для картинок идущих вниз
+function createImgUp() {
+    for (var i = 0; i < 50; i++) {
+        //Создаем картинки
+        img = document.createElement("img")
+        img.src = imgLoc + (i + 1) + ".jpg";
 
-function bigSizeImg(event) {
-    if (!event.target.closest("img")) return
-    window.cancelAnimationFrame(stop)
-    event.target.style.transform = "none"
-        // event.target.style.width = 100 + "%";
-    event.target.style.height = 95 + "vh";
-    event.target.style.position = "absolute";
-    event.target.style.top = -200 + "px";
-    event.target.style.left = 0;
-    event.target.style.zIndex = 1000;
-    event.target.cursor = "pointer";
-    container.style.overflow = "initial"
-    document.body.removeEventListener("click", bigSizeImg)
+        //Присваем 3 переменным случайное значение в диапозоне
+        xU = getRandomInRange(100, window.screen.width - 100);
+        yU = getRandomInRange(-11500, -12000);
+        zU = getRandomInRange(300, 2000);
+
+        //Размещаем картинки
+        img.style.transform = "translate3d(" + xU + "px, " + yU + "px, -" + zU + "px)";
+        img.style.cursor = "pointer"
+        container.append(img);
+        waveSrcUp.push(img)
+    }
 }
-
-function smallSizeImg(event) {
-    if (event.target.contains(section) || event.target.contains(containerMain)) {
-        window.requestAnimationFrame(yGo)
-        container.style.overflow = "hidden"
-        console.log(img)
-        document.body.addEventListener("click", bigSizeImg)
+//Если ширина экрана менее 600px прекращаем рандом и запускаем картинку снизу вверх одна за другой
+if (window.matchMedia("(max-width: 600px)").matches) {
+    function createImg() {
+        for (var i = 0; i < 50; i++) {
+            //Создаем картинки
+            img = document.createElement("img")
+            img.src = imgLoc + (i + 1) + ".jpg";
+            y = 0
+                //Размещаем картинки
+            img.style.transform = `translate(${0}px,${y}px)`
+            img.style.cursor = "pointer"
+            container.append(img);
+            waveSrc.push(img)
+        }
     }
 }
 
-function yGo() {
-    waveSrc.forEach((i) => {
-        i.style.top = `${y -= 0.07}px`
-    })
-    stop = window.requestAnimationFrame(yGo);
+//Если ширина экрана менее 600px картинки сверху вниз больше не двигаются
+if (window.matchMedia("(max-width: 600px)").matches) {
+    function createImgUp() {
+        return false
+    }
 }
 
+//События при клики на любую картинку
+document.body.addEventListener("click", bigSizeImg)
+
+//Событие при клики на крестик
+document.body.addEventListener("click", smallSizeImg)
+
+//Увеличение картинки при клики на нее
+function bigSizeImg(event) {
+
+    if (!event.target.closest("img")) return //если кликаем не на картинку - ничего не происходит
+
+    let img = document.querySelectorAll(".wrapper__gallery img")
+    img.forEach(i => i.style.opacity = 0) //Перебераем все отсальные картинки и делаем их прозрачными
+    window.cancelAnimationFrame(animationUp) //Останавливаем движение
+    window.cancelAnimationFrame(animationDown)
+    sectionTop.style.position = "absolute"
+    headerGallery.style.zIndex = 0
+    event.target.classList.add("bigImg") //Доавляем класс к картинке по которой мы кликнуле
+    event.target.style.transform = "initial"
+    picture = event.target //создаем глобальную переменную и передаем в нее картинку по корой мы кликнули
+
+    // Добавление иконки крестика
+    let close = document.createElement("div")
+    close.classList.add("infoPerson_close")
+    container.append(close)
+    close.hidden = false
+    console.log(event.target.getBoundingClientRect().top)
+    close.style.top = event.target.getBoundingClientRect().top + "px"
+    close.style.left = (container.clientWidth) - (close.getBoundingClientRect().width) + "px"
+    document.body.removeEventListener("click", bigSizeImg)
+}
+//Скрываем картинку при клике на крестик
+function smallSizeImg(event) {
+    if (!event.target.closest(".infoPerson_close")) return //Если кликнули не на крестик ничего не происходит
+    let img = document.querySelectorAll(".wrapper__gallery img")
+    img.forEach(i => i.style.opacity = 1) //Перебираем остальные картинки и делаем их опять видимыми
+    window.requestAnimationFrame(yGo); //Запускаем движение картинок
+    window.requestAnimationFrame(yUp)
+    sectionTop.style.position = "static"
+    headerGallery.style.zIndex = 1000
+    picture.classList.remove("bigImg") //Удаляем присвоенной класс открывшеся картинке
+    picture.style.transform = "initial"
+    picture.style.overflow = "hidden"
+    event.target.classList.remove("infoPerson_close") //Удаляем класс для иконки крестика
+    document.body.addEventListener("click", bigSizeImg)
+}
+
+//Функция по запуску движения картинок
+function yGo() {
+    waveSrc.forEach((i) => {
+        i.style.top = `${y -= 0.06}px `
+    })
+    animationUp = window.requestAnimationFrame(yGo);
+}
+if (window.matchMedia("(max-width: 600px)").matches) {
+    function yGo() {
+        waveSrc.forEach((i) => {
+            i.style.top = `${y -= 0.04}px `
+        })
+        animationUp = window.requestAnimationFrame(yGo);
+    }
+}
+
+function yUp() {
+    waveSrcUp.forEach((i) => {
+        i.style.top = `${yU=+yU+0.06}px `
+    })
+    animationDown = window.requestAnimationFrame(yUp);
+}
 yGo()
+yUp()
 createImg()
-
-
-// let arr = []
-
-// function preloadImage(filename) {
-//     let img = new Image();
-//     img.onload = function() {
-//         img.xPlane = getRandomInRange(-500, screenWidth - 500);
-//         img.yPlane = getRandomInRange(0, 1000);
-//         img.zPlane = getRandomInRange(300, 2000);
-
-
-//         arr.push(img.xPlane)
-//         if (Math.abs(arr[arr.length - 1] - arr[arr.length - 2]) > 300) {
-//             img.style = "transform: translate3d(" + img.xPlane + "px, " + img.yPlane + "px, -" + img.zPlane + "px)";
-//             container.appendChild(img);
-//             console.log(img.xPlane)
-//         } else {
-//             preloadImage(filename)
-
-//         }
-
-
-
-
-//     };
-//     imgLoc = "";
-//     img.src = imgLoc + filename;
-//     img.alt = "";
-//     waves[j] = img;
-//     j++;
-
-
-// }
-
-
-// function loadImages() {
-//     for (var i = 0; i < waveSrc.length; ++i) {
-//         var filename = waveSrc[i];
-//         preloadImage(filename);
-//     }
-// }
-
-// function moveImages() {
-//     waves.forEach(function(image) {
-//         image.yPlane = image.yPlane - 2;
-//         image.style.cssText = "transform: translate3d(" + image.xPlane + "px, " + image.yPlane + "px,  -" + image.zPlane + "px); z-index: " + image.zIndex;
-//         console.log("z: +" + image.xPlane)
-//     });
-//     window.requestAnimationFrame(moveImages);
-// }
-
-// var perspImages = container.getElementsByTagName("img"),
-//     screenWidth = window.screen.width,
-//     screenHeight = window.screen.height;
-
-// loadImages();
-// window.requestAnimationFrame(moveImages);
+createImgUp()
